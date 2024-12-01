14661c81319e6aa0b8e9ff6b9287c251d8ede591
KartonM <mkozubek@protonmail.com>
2024-12-02 00:10:36 +0100

    Day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,23 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  def parse_input(input) do
+    lines =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line -> String.split(line, "   ") |> Enum.map(&String.to_integer/1) end)
+
+    left = Enum.map(lines, fn [x, _] -> x end)
+    right = Enum.map(lines, fn [_, y] -> y end)
+    {left, right}
   end
 
-  def part2(_args) do
+  def part1(input) do
+    {left, right} = parse_input(input) |> then(fn {l, r} -> {Enum.sort(l), Enum.sort(r)} end)
+    Enum.zip(left, right) |> Enum.map(fn {x, y} -> abs(x - y) end) |> Enum.sum()
+  end
+
+  def part2(input) do
+    {left, right} = parse_input(input)
+    frequencies = Enum.frequencies(right)
+    Enum.map(left, fn x -> x * Map.get(frequencies, x, 0) end) |> Enum.sum()
   end
 end
