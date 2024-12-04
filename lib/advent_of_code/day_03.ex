a6b926b9b99db90b3af80a21acfad6961028bfc1
KartonM <mkozubek@protonmail.com>
2024-12-04 01:39:29 +0100

    Day 3

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,7 +1,15 @@
 defmodule AdventOfCode.Day03 do
-  def part1(_args) do
+  def part1(input) do
+    Regex.scan(~r/mul\(\d+,\d+\)/, input)
+    |> Enum.map(&Regex.scan(~r/\d+/, hd(&1)))
+    |> Enum.map(fn [[x], [y]] -> String.to_integer(x)*String.to_integer(y) end)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(input) do
+    String.split(input, "do()")
+    |> Enum.map(fn s -> String.split(s, "don't()") |> hd() end)
+    |> Enum.join()
+    |> part1()
   end
 end
