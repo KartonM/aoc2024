0253196af32fde672254fb8ed127fdcd2e025742
KartonM <mkozubek@protonmail.com>
2024-12-11 18:27:50 +0100

    Day 11

--- lib/advent_of_code/day_11.ex
+++ lib/advent_of_code/day_11.ex
@@ -1,7 +1,47 @@
 defmodule AdventOfCode.Day11 do
-  def part1(_args) do
+  use Memoize
+
+  def parse_input(input) do
+    input
+    |> String.trim("\n")
+    |> String.split(" ")
+    |> Enum.map(&String.to_integer/1)
   end
 
-  def part2(_args) do
+  def split(string) do
+    middle_index = div(String.length(string), 2)
+
+    {String.slice(string, 0, middle_index) |> String.to_integer(),
+     String.slice(string, middle_index, String.length(string)) |> String.to_integer()}
+  end
+
+  defmemo count_stones_after_blinks(_, 0), do: 1
+
+  defmemo count_stones_after_blinks(stone, n) do
+    string = Integer.to_string(stone)
+
+    cond do
+      stone == 0 ->
+        count_stones_after_blinks(1, n - 1)
+
+      rem(String.length(string), 2) == 0 ->
+        {left, right} = split(string)
+        count_stones_after_blinks(left, n - 1) + count_stones_after_blinks(right, n - 1)
+
+      true ->
+        count_stones_after_blinks(stone * 2024, n - 1)
+    end
+  end
+
+  def part1(input) do
+    parse_input(input)
+    |> Enum.map(&count_stones_after_blinks(&1, 25))
+    |> Enum.sum()
+  end
+
+  def part2(input) do
+    parse_input(input)
+    |> Enum.map(&count_stones_after_blinks(&1, 75))
+    |> Enum.sum()
   end
 end
--- lib/mix/tasks/d11.p2.ex
+++ lib/mix/tasks/d11.p2.ex
@@ -5,7 +5,8 @@ defmodule Mix.Tasks.D11.P2 do
 
   @shortdoc "Day 11 Part 2"
   def run(args) do
-    input = nil
+    {:ok, _} = Application.ensure_all_started(:memoize)
+    input = AdventOfCode.Input.get!(11, 2024)
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_2: fn -> input |> part2() end}),
